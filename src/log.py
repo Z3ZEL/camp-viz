5067fedaa6cf4dc10de0e26ce455782f1e288047
Z3ZEL <estebanrodriguez17780@gmail.com>
2023-05-17 17:27:59 +0200

    logger + .vscode

---
+++ src/log.py
@@ -0,0 +1,16 @@
+class Log:
+    def __init__(self, verbose=False,header="[LOG]"):
+        self.verbose = verbose
+        self.header = header
+    def print(self, message):
+        '''Print message if verbose is true'''
+        if self.verbose:
+            print(self.header + " : ",end='')
+            print(message)
+    def printAnyway(self, message):
+        '''Print message anyway'''
+        print(self.header + " : ",end='')
+        print(message)
+    def error(self, message):
+        '''Print error with red color'''
+        print("\033[91m" + "[ERROR]" + message + "\033[0m")
